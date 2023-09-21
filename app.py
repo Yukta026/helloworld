a2ef6e7168e6b6de899c0cc33a6893df96a6d24b
GURNANI YUKTA RAJKUMAR <143731993+Yukta026@users.noreply.github.com>
2023-09-21 19:21:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,20 +2,13 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 
-filename = 'new_more_popular.pkl'
-new_more_popular_df = pickle.load(open(filename,'rb'))
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html'
-                           # ,book_name = list(new_more_popular_df['Book-Title'].values),
-                           # author=list(new_more_popular_df['Book-Author'].values),
-                           # image=list(new_more_popular_df['Image-URL-M'].values),
-                           # votes=list(new_more_popular_df['num_ratings'].values),
-                           # rating=list(new_more_popular_df['avg_ratings'].values)
-                          )
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
