ac7fcbeed87718f3bf3e03ab592be2a39bbbeb75
GURNANI YUKTA RAJKUMAR <143731993+Yukta026@users.noreply.github.com>
2023-09-22 16:38:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask,render_template,request
+import os
 import pickle
 import joblib
 import numpy as np
 import pandas as pd
 
+
 app = Flask(__name__)
 
-filename = 'popular.pkl'
-popular = pickle.load(open(filename, 'rb'))
+popular_path = os.path.join(os.path.dirname(__file__), 'popular.pkl')
+
+with open(popular_path, 'rb') as file:
+    popular = pickle.load(file)
 
 @app.route('/')
 def index():
