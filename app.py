a895029f7ff6be00366265830230e980c6909b5a
GURNANI YUKTA RAJKUMAR <143731993+Yukta026@users.noreply.github.com>
2023-09-22 23:35:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,49 @@ similarity_scores = load_pickle(similarity_scores_path)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           book_name = list(popular['Book-Title'].values),
+                           author=list(popular['Book-Author'].values),
+                           image=list(popular['Image-URL-M'].values),
+                           votes=list(popular['num_ratings'].values),
+                           rating=list(popular['avg_ratings'].values)
+                           )
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/book-recommender-system')
+def code():
+    return render_template('book-recommender-system.html')
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+
+
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    index = np.where(pt.index == user_input)[0][0]
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:6]
+    data = []
+    for i in similar_items:
+        item = []
+        #         print(pt.index[i[0]])
+        # temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
+        item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
+
+        data.append(item)
+    print(data)
+
+    return render_template('recommend.html',data=data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
