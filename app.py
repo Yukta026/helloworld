b62d2f1773ceb523ca1df0e743f7c39b042c1dcd
GURNANI YUKTA RAJKUMAR <143731993+Yukta026@users.noreply.github.com>
2023-09-22 16:52:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,14 +4,20 @@ import pickle
 import joblib
 import numpy as np
 import pandas as pd
-
+import logging
 
 app = Flask(__name__)
 
-popular_path = os.path.join(os.path.dirname(__file__), 'popular.pkl')
+logging.basicConfig(filename='app.log', level=logging.DEBUG)
 
-with open(popular_path, 'rb') as file:
-    popular = pickle.load(file)
+try:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(script_dir, 'popular.pkl')
+
+    with open(model_path, 'rb') as file:
+        popular = pickle.load(file)
+except Exception as e:
+    logging.error(f"Error loading pickle file: {str(e)}")
 
 @app.route('/')
 def index():
