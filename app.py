997829962d2c9b3c7e5047f9acb316b4bff5127f
GURNANI YUKTA RAJKUMAR <143731993+Yukta026@users.noreply.github.com>
2023-09-24 17:18:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,15 @@ logging.basicConfig(filename='app.log', level=logging.DEBUG)
 try:
     script_dir = os.path.dirname(os.path.abspath(__file__))
     model_path = os.path.join(script_dir, 'popular.pkl')
+    pt_path = os.path.join(script_dir, 'pt.pkl')
+    similarity_scores_path = os.path.join(script_dir, 'similarity_scores.pkl')
     
     with open(model_path, 'rb') as file:
             popular = pickle.load(file)
+    with open(pt_path, 'rb') as file:
+            pt = pickle.load(file)
+    with open(similarity_scores_path, 'rb') as file:
+            similarity_scores = pickle.load(file)
 except Exception as e:
         logging.error(f"Error loading pickle file: {str(e)}")
         
